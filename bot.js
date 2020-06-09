c707c215d9a60f2da7f4cc2a33bb111d4e1d1a18
Eric Rovell <ericrovell@gmail.com>
2020-06-09 14:15:24 +0300

    main class

--- bot.js
+++ bot.js
@@ -1,29 +1,37 @@
 import TelegramBot from "node-telegram-bot-api";
+import Auth from "./auth.js";
+
+import ConversationAddEcontwitt from "./econtwitt/addEcontwitt.js";
 
 export default class Bot {
-  constructor(token) {
+  constructor({ token, dbClient }) {
     this.client = new TelegramBot(token, { polling: true });
-  }
+    this.dbClient = dbClient;
+    this.allowedChatIDs = new Set();
 
-  init() {
-    this.client.onText(/\/echo (.+)/, async (message, match) => {
-      const { id } = message.chat;
-      const echo = match[1];
     
-      this.client.sendMessage(
-        id,
-        `~${echo}~`,
-        { parse_mode: "MarkdownV2" }
-      );
-
-      this.client.sendMessage(id, "reply me :)", {
-        reply_markup: {
-          force_reply: true,
-          keyboard: [ ["Русский", "English"] ]  
-        }
-      });
-    });
   }
+
+  async init() {
+    this.client.onText(/\/start/, async message => {
+      const chatID = message.chat.id;
+
+      const user = new Auth({
+        chatID,
+        bot: this.client,
+        dbClient: this.dbClient
+      });
+
+      const permission = await user.auth();
+      if (permission) {
+        this.allowedChatIDs.add(chatID);
+      }
+    });
+
+  }
+
+  //const addEcontwitt = new ConversationAddEcontwitt(telegramBot.client, dbClient);
+  //addEcontwitt.init();
 
 }
 
@@ -32,25 +40,6 @@ export default class Bot {
   const twittID = match[1];
 
   console.log(twittID);
-
-  const db = await client.connect();
-  const twitt = await db
-    .collection("blog.econtwitts")
-    .findOne({ _id: ObjectID(twittID) });
-
-  bot.sendMessage(id, `Hello! Here is the data: ${JSON.stringify(twitt, null, 2)}`);
-}); */
-
-/* bot.onText(/\/econtwitt/, async (message, match) => {
-  const { id } = message.chat;
-  const twittID = match[1];
-
-  // ask a language
-  bot.sendMessage(message.chat.id, "Econtwitt's language:", {
-    reply_markup: {
-      "keyboard": [ ["ru"], ["en"] ]
-    }
-  });
 
   const db = await client.connect();
   const twitt = await db
