59286c56aa132ef12d861f5d80fd76cfef7e2b2a
Eric Rovell <ericrovell@gmail.com>
2020-06-12 19:56:03 +0300

    moved all code inside the class

--- server.js
+++ server.js
@@ -2,11 +2,7 @@ import dotenv from "dotenv";
 import DBClient from "./db.js";
 import Bot from "./bot.js";
 
-import ConversationAddEcontwitt from "./econtwitt/addEcontwitt.js";
-import ConversationGetEcontwitt from "./econtwitt/getEcontwitt.js";
-
 dotenv.config();
-
 
 const telegramBot = new Bot({
   token: process.env.TOKEN,
@@ -17,27 +13,3 @@ const telegramBot = new Bot({
 });
 
 telegramBot.init();
-
-telegramBot.client.onText(/\/econtwitt (.+)/, async (message, match) => {
-  const chatID = message.chat.id;
-  const command = match[1];
-  
-  switch(command) {
-    case "add": {
-      new ConversationAddEcontwitt({
-        bot: telegramBot.client,
-        dbClient: telegramBot.dbClient,
-        chatID
-      });
-      break;
-    }
-    case "read": {
-      new ConversationGetEcontwitt({
-        bot: telegramBot.client,
-        dbClient: telegramBot.dbClient,
-        chatID
-      });
-      break;
-    }
-  }
-});
