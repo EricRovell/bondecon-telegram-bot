edb9c4211d6db126f6295444a4b7b5ec52431627
Eric Rovell <ericrovell@gmail.com>
2020-06-12 19:54:25 +0300

    the function moved into the class

--- econtwitt/render.js
+++
@@ -1,7 +0,0 @@
-export default function renderEcontwitt({ _id, body, keywords = [] }) {
-  const parsedKeywords = keywords
-    .map(keyword => `#${keyword.replace(/\s/g, "_")}`)
-    .join(" ");
-
-  return (`ID: <b>${_id}</b>\n\n<pre>${body}</pre>\n\n<i>${parsedKeywords}</i>`);
-}
