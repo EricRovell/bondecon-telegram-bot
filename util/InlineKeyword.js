602eda13fda8e187d005e2c5579ed2219cebe281
Eric Rovell <ericrovell@gmail.com>
2020-06-13 15:09:16 +0300

    updated inline keyboard constructor

--- util/InlineKeyword.js
+++ util/InlineKeyword.js
@@ -1,16 +1,36 @@
-export default function inlineKeyboard(options) {
+/* const options = [
+  ["Text", "value"],
+  [
+    ["Text", "value"],
+    ["Text", "value"],
+    ["Text", "value"],
+  ]
+] */
 
+function inlineButtons(options) {
+
+  const inlineButton = data => {
+    const [ text, command ] = data;
+    return {
+      "text": text,
+      callback_data: JSON.stringify({ command })
+    }
+  };
+
+  return options.map(option => {
+    if (option.every(value => typeof value === "string")) {
+      return inlineButton(option);
+    } else {
+      return inlineButtons(option);
+    }
+  });
+}
+
+export default function inlineKeyboard(options, reply) {
   return {
     reply_markup: {
-      inline_keyboard: [
-        options.map(option => ({
-          text: option.text,
-          callback_data: JSON.stringify({
-            "command": option.command,
-            "value": option.value
-          })
-        }))
-      ]
+      ...(reply ? { "force_reply": true } : []),
+      inline_keyboard: inlineButtons(options)
     }
   };
 }
