aae3a62194e4c45c057008cac9e114cf4c2bb96e
Eric Rovell <ericrovell@gmail.com>
2020-06-13 15:10:06 +0300

    econtwitt command constructor

--- econtwitt/EcontwittCommand.js
+++ econtwitt/EcontwittCommand.js
@@ -1,67 +1,72 @@
 import econtwittCreate from "./dialogue/econtwittCreate.js";
 import econtwittFind from "./dialogue/econtwittFind.js";
 
-import inlineKeyboard from "../util/InlineKeyword.js";
+import inlineKeyboard from "../util/inlineKeyword.js";
 
 export default class EcontwittCommand {
   constructor({ bot, dbClient, chatID }) {
     this.bot = bot;
     this.dbClient = dbClient;
-  
     this.chatID = chatID;
-    this.messageID;
+    this.messageID = null;
     this.econtwitt;
 
-    this.callbacks();
     this.message();
   }
 
   async message() {
     const message = "Create a new record or find records to work with them:";
     const inlineOptions = [
-      { text: "\u{1F4DD} Create", "command": "create", value: "new" },
-      { text: "\u{1F50D} Find", "command": "find", value: "find" },
+      [
+        [ "\u{1F4DD} Create", "create" ],
+        [ "\u{1F50D} Find", "find" ]
+      ]
     ];
 
     const { message_id } = await this.bot.sendMessage(this.chatID, message, inlineKeyboard(inlineOptions));
     this.messageID = message_id;
+
+    let reply = await new Promise(resolve => {
+      this.bot.on("callback_query", callbackQuery => {
+        const { command } = JSON.parse(callbackQuery.data);
+        resolve(command);  
+      });
+    });
+
+    switch (reply) {
+      case "create":
+        this.create(); break;
+      case "find":
+        this.findOption(); break;
+    }
   }
 
-  callbacks() {
-    this.bot.on("callback_query", callbackQuery => {
-      const { command } = JSON.parse(callbackQuery.data);
-
-      if (command === "create") {
-        new econtwittCreate({
-          bot: this.bot,
-          dbClient: this.dbClient,
-          chatID: this.chatID
-        });
-      }
-
-      if (command === "find") {
-        const message = "Choose the criteria for a search:"
-        const inlineOptions = [
-          { text: "ID", "command": "create", value: "new" },
-          { text: "Date", "command": "read", value: "read" },
-          { text: "Language", "command": "create", value: "new" },
-          { text: "Keywords", "command": "read", value: "read" },
-        ];
-        
-        this.bot.editMessageText(message, {
-          message_id: this.messageID,
-          chat_id: this.chatID,
-          ...inlineKeyboard(inlineOptions)
-        });
-        /* new econtwittFind({
-          bot: this.bot,
-          dbClient: this.dbClient,
-          chatID: this.chatID
-        }); */
-      }
-
+  create() {
+    new econtwittCreate({
+      bot: this.bot,
+      dbClient: this.dbClient,
+      chatID: this.chatID,
+      messageID: this.messageID
     });
-    
+  }
+
+  findOption() {
+    const message = "Choose the criteria for a search:"
+    const inlineOptions = [
+      [[ "ID", "id" ]],
+      [
+        [ "Date", "date" ],
+        [ "Language", "lang" ],
+        [ "Keywords", "keywords" ]
+      ],
+      [[ "Back", "back" ], [ "Search", "search" ]]
+    ];
+  
+    this.bot.editMessageText(message, {
+      message_id: this.messageID,
+      chat_id: this.chatID,
+      ...inlineKeyboard(inlineOptions)
+    });
   }
 
 }
