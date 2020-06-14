d82106fa7d3212aa2a239c197e9350d6325759ab
Eric Rovell <ericrovell@gmail.com>
2020-06-14 16:38:33 +0300

    find econtwitt dialogue

--- econtwitt/dialogue/econtwittFind.js
+++ econtwitt/dialogue/econtwittFind.js
@@ -1,41 +1,136 @@
 import Econtwitt from "../Econtwitt.js";
+import inlineKeyboard from "../../util/inlineKeyword.js";
+import question from "../../util/question.js";
+import questionInline from "../../util/questionInline.js";
 
 export default class EcontwittFind {
-  constructor({ bot, dbClient, chatID }) {
+  constructor({ bot, dbClient, chatID, messageID }) {
     this.bot = bot;
     this.dbClient = dbClient;
     this.chatID = chatID;
+    this.messageID = messageID;
+    this.callbackID = null;
+    this.query = {};
 
-    this.econtwittID = null;
-
-    this.doSurvey();
+    this.init();
   }
 
-  async doSurvey() {
-    await this.askID();
-    await this.queryDB();
-  }  
+  init() {
+    const message = "Please, provide some information:"
+    const inlineOptions = [
+      [
+        [ "\u{1F4C5} Date", "date" ],
+        [ "\u{1F310} Language", "lang" ],
+        [ "\u{1F5DD} Keywords", "keywords" ]
+      ],
+      [[ "\u{2728} ID", "id" ]],
+      [
+        [ "\u{274C} Cancel", "cancel" ],
+        [ "\u{1F195} Reset", "reset" ],
+        [ "\u{1F50D} Search", "search" ]
+      ]
+    ];
+  
+    this.bot.editMessageText(message, {
+      message_id: this.messageID,
+      chat_id: this.chatID,
+      ...inlineKeyboard(inlineOptions)
+    });
+
+    /* await new Promise(resolve => { */
+      this.bot.on("callback_query", async callbackQuery => {
+        const { command } = JSON.parse(callbackQuery.data);
+        this.callbackID = callbackQuery.id;
+
+        switch(command) {
+          case "id":
+            this.askID(); break;
+          case "date":
+            this.askDate(); break;
+          case "lang":
+            this.askLang(); break;
+          case "keywords":
+            this.askKeywords(); break;
+          case "cancel":
+            this.finishDialogue(); break;
+          case "reset":
+            this.query = {}; break;
+          case "search":
+            this.queryDB(); break;
+        }
+
+        /* resolve(reply); */
+      });
+    /* }); */
+  }
+
+  async finishDialogue() {
+    await this.bot.deleteMessage(this.chatID, this.messageID);
+  }
 
   async askID() {
-    const { message_id: questionID } = await this.bot.sendMessage(this.chatID, "Please, provide an ID:", {
-      reply_markup: { "force_reply": true }
-    });
-    let { text, message_id: replyID } = await new Promise(resolve => {
-      this.bot.onReplyToMessage(this.chatID, questionID, resolve);
+    const id = await question({
+      bot: this.bot,
+      chatID: this.chatID,
+      message: "Please, provide an ID:"
     });
 
-    this.econtwittID = text;
-    await this.bot.deleteMessage(this.chatID, questionID);
-    await this.bot.deleteMessage(this.chatID, replyID);
+    this.bot.answerCallbackQuery(this.callbackID, {
+      text: "Got an ID."
+    });
+    this.query._id = this.dbClient.ID(id);
+  }
+
+  async askLang() {
+    const reply = await questionInline({
+      bot: this.bot,
+      chatID: this.chatID,
+      message: "Please, provide an ID:",
+      options: [
+        [[ "Русский", "ru" ], [ "English", "en" ]]
+      ]
+    });
+
+    this.bot.answerCallbackQuery(this.callbackID, {
+      text: `Language set to ${reply}.`
+    });
+    this.query.lang = reply;
+  }
+
+  async askKeywords() {
+    const reply = await question({
+      bot: this.bot,
+      chatID: this.chatID,
+      message: "Please, provide an ID:"
+    });
+
+    // to select documents where all such keywords are present
+    this.query.keywords = {
+      $all: reply.split(",").map(keyword => keyword.trim().toLowerCase())
+    };
+  }
+
+  async askDate() {
+    const reply = await question({
+      bot: this.bot,
+      chatID: this.chatID,
+      message: "Please, provide a day date as YYYY-MM-DD:"
+    });
+
+    this.query.date = {
+      $gte: new Date(new Date(reply).setHours(0, 0, 0)).toISOString(),
+      $lt: new Date(new Date(reply).setHours(23, 59, 59)).toISOString()
+    };
   }
 
   async queryDB() {
     const db = await this.dbClient.connect();
 
     try {
+      console.log(this.query);
       const result = await db
         .collection("blog.econtwitts")
-        .findOne({ _id: this.dbClient.ID(this.econtwittID) });
+        .findOne({ ...this.query });
       
       if (result) {
         const econtwitt = new Econtwitt(result);
@@ -43,7 +138,9 @@ export default class EcontwittFind {
           { parse_mode: "HTML" }
         );
       } else {
-        await this.bot.sendMessage(this.chatID, "Nothing has been found, sorry.");
+        await this.bot.answerCallbackQuery(this.callbackID, {
+          text: "Nothing has been found..."
+        });
       }
       
     } catch (error) {
