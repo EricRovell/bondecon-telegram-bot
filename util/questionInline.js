a53c863df98862bf9c99ae48a873eb75d2435452
Eric Rovell <ericrovell@gmail.com>
2020-06-15 13:55:49 +0300

    now it is possible to use existing message for a question

--- util/questionInline.js
+++ util/questionInline.js
@@ -1,17 +1,37 @@
 import inlineKeyboard from "../util/inlineKeyword.js";
 
-export default async function question({ bot, chatID, message, options }) {
+export default async function question({ bot, chatID, messageID, message, options }) {
+
+  // await for callback reply
+  async function callbackReply() {
+    return await new Promise(resolve => {
+      bot.on("callback_query", callbackQuery => {
+        const { command } = JSON.parse(callbackQuery.data);
+        /* bot.answerCallbackQuery(callbackQuery.id, {
+          text: "The language has been selected"
+        }); */
+        resolve(command);  
+      });
+    });
+  }
+
+  // if messageID is provided, this message is edited to make up a question
+  if (messageID) {
+    await bot.editMessageText(message, {
+      message_id: messageID,
+      chat_id: chatID,
+      ...inlineKeyboard(options)
+    });
+
+    return await callbackReply();
+  }
+
+  // no messageID -> completely new message for question, deleted afterwards
   const { message_id: questionID } = await bot.sendMessage(chatID, message, {
     ...inlineKeyboard(options)
   });
 
-  const reply = await new Promise(resolve => {
-    bot.on("callback_query", async callbackQuery => {
-      const { command } = JSON.parse(callbackQuery.data);
-      resolve(command); 
-    });
-  });
-
+  const reply = await callbackReply();
   await bot.deleteMessage(chatID, questionID);
   return reply;
 }
