74f53d3acdb709e0f3ff60fcec1ee25f3fef68fb
Eric Rovell <ericrovell@gmail.com>
2020-06-22 18:47:17 +0300

    working on more confortable auth

--- src/Bot.js
+++ src/Bot.js
@@ -9,15 +9,28 @@ export default class Bot {
   constructor({ token, dbClient }) {
     this.client = new TelegramBot(token, { polling: true });
     this.dbClient = dbClient;
-    this.#allowedChatIDs = new Set();
+    this.#allowedChatIDs = null;
   }
 
   async init() {
+    // get allowed IDs from DB
+    await this.getAllowedIDs();
+
+    // polling errors
     this.client.on("polling_error", error => console.log(error));
 
+    // start command init
     this.client.onText(/\/start/, async message => {
+      
+    });
+
+    // login command init
+    this.client.onText(/\/login/, async message => {
+      const userID = message.from.id;
       const chatID = message.chat.id;
+
       const user = new Auth({
+        userID,
         chatID,
         bot: this.client,
         dbClient: this.dbClient
@@ -30,14 +43,30 @@ export default class Bot {
     });
   }
 
+  // getting allowed userIDs from DB which allowed to interact with bot
+  // run once on bot start
+  async getAllowedIDs() {
+    const allowedIDs = await Auth.getAllowedIDs(this.dbClient);
+    this.#allowedChatIDs = new Set(
+      allowedIDs.map(({ userID }) => userID)
+    );
+  }
+
+  // validate user by userID
+  userValid(userID, chatID) {
+    const check = this.#allowedChatIDs.has(userID);
+    if (!check) {
+      this.client.sendMessage(chatID, "Please, login: /login");
+    }
+    return check;
+  }
+
   commandEcontwitt() {
     this.client.onText(/\/econtwitt/, async message => {
       const chatID = message.chat.id;
-      // validate chatID
-      if (!this.#allowedChatIDs.has(chatID)) {
-        this.client.sendMessage(chatID, "Please, login: type /start");
-        return;
-      };
+      const userID = message.from.id;
+      
+      if (!this.userValid(userID, chatID)) return;
 
       new EcontwittCommand({
         bot: this.client,
